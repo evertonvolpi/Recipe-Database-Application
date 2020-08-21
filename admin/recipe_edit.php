78583d5132ec2b8e8b2a024a03789b7b12ecd785
evolpi <everton.volpi@gmail.com>
2020-08-21 16:47:52 -0400

    Style/responsiveness of 'edit recipe' page

--- admin/recipe_edit.php
+++ admin/recipe_edit.php
@@ -41,11 +41,16 @@ if(is_post_request()) {
 
     <form action="<?php echo url_for('/admin/recipe_edit.php?id=' . $id); ?>" method="post">
         
+    <div class="sections_edit_recipe">
         <!-- N A M E -->
         
         <label for="recipe_name">Recipe Name</label></br>
         <input type="text" name="recipe_name" id="recipe_name" value="<?php echo $recipe_instructions['name']; ?>" required></br>
-        
+    
+    </div>
+
+    <div class="sections_edit_recipe">
+
         <!-- C A T E G O R I E S -->
     
         <label for="categories_list">Select the categories</label></br>
@@ -70,7 +75,9 @@ if(is_post_request()) {
         <?php mysqli_free_result($recipe_categories); ?>
             
         <input type="text" name="cat_list" id="cat_list" class="hidden" value=""/>
-            
+    </div>
+
+    <div class="sections_edit_recipe">        
         <!-- I N G R E D I E N T S -->
             
         <label for="ingredients_list">Select new ingredient</label></br>
@@ -103,14 +110,16 @@ if(is_post_request()) {
         <?php mysqli_free_result($recipe_ingredients); ?>
             
         <input type="text" name="ing_list" id="ing_list" class="hidden" value=""/>
-            
+    </div>
+
+    <div class="sections_edit_recipe">
         <!-- I N S T R U C T I O N S -->
             
         <label for="recipe_instructions_form">Recipe Instructions</label></br>
         <textarea name="recipe_instructions" id="recipe_instructions" cols="30" rows="10">
             <?php echo h($recipe_instructions['content']); ?>
         </textarea></br>
-            
+    </div>     
             
         <!-- S U B M I T -->
         <button type="submit" id="submit">Submit</button>
